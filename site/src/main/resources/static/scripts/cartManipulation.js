968f8b08a32f22c5833aba9557ad9b1d78e3c109
ssserchenko <se.volchenko@ya.ru>
2022-12-23 10:19:28 +0300

    CartManipulation in process

--- site/src/main/resources/static/scripts/cartManipulation.js
+++ site/src/main/resources/static/scripts/cartManipulation.js
@@ -1,1 +1,22 @@
-localStorage.setItem('test', 1);
+if (localStorage.getItem('cart') == null) {
+    var cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addProductToCart(productId) {
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(productId)
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+function removeProductFromCart(productId) {
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+    var filtered = cart.filter(function(currProductId){
+        return productId !== currProductId;
+    });
+    localStorage.setItem('cart', JSON.stringify(filtered))
+}
+
+function containsInCart(productId) {
+
+}
